124a6b60dd67d54ce121fb617b4bf7c936803773
Blebyshek <galkrot@mail.ru>
2023-09-28 18:14:33 +0700

    Использование lombok
    @Getter
    @Setter вместо ручных

--- bot/src/main/java/com/example/bot/model/User.java
+++ bot/src/main/java/com/example/bot/model/User.java
@@ -2,8 +2,12 @@ package com.example.bot.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity(name="usersDataTable")
+@Getter
+@Setter
 public class User {
     @Id
     private Long chatId;
@@ -18,69 +22,7 @@ public class User {
 
 
 
-    public Integer getCurrentIndex() {
-        return currentIndex;
-    }
 
-    public void setCurrentIndex(Integer currentIndex) {
-        this.currentIndex = currentIndex;
-    }
-
-    public UserState getState() {
-        return state;
-    }
-
-    public void setState(UserState state) {
-        this.state = state;
-    }
-
-    public Long getChatId() {
-        return chatId;
-    }
-
-    public void setChatId(Long chatId) {
-        this.chatId = chatId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 
 }
